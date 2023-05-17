9d0831249e3154f771adaba8dd4da65be28f5d38
Christoph Pilz <christoph.pilz@v2c2.at>
2023-05-17 12:53:13 +0200

    Update docker_demo_build_and_run.sh
    [fix]
    - fixed wrong network name in docker run

--- docker_demo_build_and_run.sh
+++ docker_demo_build_and_run.sh
@@ -1,3 +1,3 @@
 docker build -t vehicle_captain/cpp_sender_client:0.1 -f Dockerfiles/Dockerfile.demo .
 docker network create vc-routing-bridge
-docker run -t -d --name cpp_sender-demo --network=routing-bridge vehicle_captain/cpp_sender_client:0.1
+docker run -t -d --name cpp_sender-demo --network=vc-routing-bridge vehicle_captain/cpp_sender_client:0.1
